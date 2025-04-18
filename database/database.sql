81eef23c0b0a857eb2eef403843862715fdd9773
Franco <antonfelixfranco@gmail.com>
2025-04-18 12:18:43 -0500

    base de datos creada

---
+++ database/database.sql
@@ -0,0 +1,30 @@
+CREATE DATABASE BIBLIOTECA;
+USE BIBLIOTECA;
+
+CREATE TABLE LIBRO(
+    id                  INT PRIMARY KEY AUTO_INCREMENT,
+    titulo              VARCHAR(70),
+    autor               VARCHAR(100),
+    genero              ENUM('Biografia','Poesia','Drama','Cuento','Fabula'),
+    fechaPublicacion    DATE,
+    isbn                VARCHAR(50),
+    disponible          BOOLEAN
+)ENGINE=INNODB;
+
+INSERT INTO LIBRO(titulo,autor,genero,fechaPublicacion,isbn,disponible)VALUES      
+                ('El principito','Antoine de Saint-Exupéry','fantasía','1943-04-06','9780156012195',true);
+
+CREATE VIEW vista_libros AS
+SELECT 
+    id,
+    titulo,
+    autor,
+    genero,
+    FechaPublicacion,
+    isbn,
+    CASE 
+        WHEN disponible = 1 THEN 'Disponible'
+        WHEN disponible = 0 THEN 'No disponible'
+    END AS disponibilidad
+FROM LIBRO;
+SELECT*FROM LIBRO;
